c4d5d735dea4d70b30412bc650b3c83b7801e29c
Denis Cohen <denis.cohen.132@gmail.com>
2025-03-04 15:46:32 +0100

    Updated shiny function

--- inst/shiny/shiny_point_estimate/app.R
+++ inst/shiny/shiny_point_estimate/app.R
@@ -99,7 +99,7 @@ ui <- shiny::fluidPage(
   shiny::fluidRow(
     tags$div(
       style = "position: relative;",
-      ggiraph::ggiraphOutput(outputId = "triangularmatrix", height = "900px")
+      ggiraph::girafeOutput(outputId = "triangularmatrix", height = "900px")
     )
   )
 )
